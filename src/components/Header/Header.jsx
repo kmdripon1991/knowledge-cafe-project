6d1c12c039d68d8b51c80000e2e515b7bf95acfc
kmdripon1991 <kmdripon1991@gmail.com>
2024-09-21 21:33:56 +0600

    header image updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center border-b-2 border-gray-400 my-4 py-4">
       <h1 className="text-4xl tracking-wider font-semibold">Knowledge Cafe</h1>
-      <img src="../../../public/images/profile.png" alt="" />
+      <img src="/images/profile.png" alt="" />
     </div>
   );
 }
